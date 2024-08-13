500d96c5ede13b61a75052956421ad0b4ba80007
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-08-13 20:32:50 -0300

    Update Selection Sort com Lista encadeada.py

--- Exercício 2/Selection Sort com Lista encadeada.py
+++ Exercício 2/Selection Sort com Lista encadeada.py
@@ -1,1 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(data)
+
+    def selectionSort(self):
+        if not self.head:
+            return
+
+        current = self.head
+        while current:
+            minNode = current
+            nextNode = current.next
+            while nextNode:
+                if nextNode.data < minNode.data:
+                    minNode = nextNode
+                nextNode = nextNode.next
+            if minNode != current:
+                current.data, minNode.data = minNode.data, current.data
+            current = current.next
+
+    def printList(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+def main():
+    array = LinkedList()
+    n = int(input("Digite a quantidade de elementos:"))
+    for i in range(n):
+        element = int(input("Digite o elemento:"))
+        array.insert(element)
+
+    print("-----------------")
+    print("Lista original:")
+    array.printList()
+    array.selectionSort()
+    print("-----------------")
+    print("Lista ordenada:")
+    array.printList()
+
+if __name__ == "__main__":
+    main()
