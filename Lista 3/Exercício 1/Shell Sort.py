4daafbc6afd663a3c44e8352f4bba4e4a78e0341
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-09-01 22:38:52 -0300

    Create Shell Sort.py

---
+++ Lista 3/Exercício 1/Shell Sort.py
@@ -0,0 +1,33 @@
+def shellSort(list):
+    nCount=len(list)//2
+    while nCount>0:
+        for startIndex in range(nCount):
+            jumpInsertionSort(list,startIndex,nCount)
+        nCount=nCount//2
+    return list
+
+def jumpInsertionSort(list,start,jump):
+    for i in range(start+jump,len(list),jump):
+        key=list[i]
+        j=i
+        while j>=jump and list[j-jump]>key:
+            list[j]=list[j-jump]
+            j-=jump
+        list[j]=key
+    return list
+
+def main():
+    data = []
+    n = int(input("Digite a quantidade de elementos:"))
+    for i in range(n):
+        element = int(input("Digite o elemento:"))
+        data.append(element)
+
+    print("-----------------")
+    print("Lista Original:", data)
+    shellSort(data)
+    print("-----------------")
+    print("Lista Ordenada:", data)
+
+if __name__ == "__main__":
+    main()
